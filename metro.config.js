568b9cc171723097bdedafabdbdf59e944f6dfb0
96993175 <om67156715@gmail.com>
2025-09-14 08:26:52 +0530

    Fix PagerView Web Compatibility Error

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+// Add web-specific resolver for react-native-pager-view
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+config.resolver.alias = {
+  ...config.resolver.alias,
+  'react-native-pager-view': require.resolve('./components/PagerViewWeb'),
+};
+
+// Platform-specific extensions
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'web.js', 'web.ts', 'web.tsx'];
+
+module.exports = config;
